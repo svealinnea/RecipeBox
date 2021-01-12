63dbe8f6308009ee8604cca9d514bfd2bde7d288
Svea Wade <svealinneawade@gmail.com>
2021-01-12 11:07:13 -0800

    adding identity
    Co-authored-by: Garrett Dean Brown <gman9mm@live.com>

--- RecipeBox/Models/RecipeBoxContext.cs
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -1,15 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-//Identifying the Database Schema
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace RecipeBox.Models
 {
   public class RecipeBoxContext : DbContext
   {
-    public virtual DbSet<Category> Categories { get; set; } //DBSets are new tables being created. 
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Recipe> Recipes { get; set; }
 
-    public DbSet<CategoryRecipe> CategoryRecipe{ get; set; }
+    public DbSet<CategoryRecipe> CategoryRecipe { get; set; }
 
-    public RecipeBoxContext(DbContextOptions options) : base(options) { } 
+    public RecipeBoxContext(DbContextOptions options) : base(options) { }
   }
 }
